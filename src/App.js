60cf1b77bd74022ed2d310c2d2e596d842bd7d21
Mani Chandan Gujja <gujjamanichandan@gmail.com>
2025-01-05 09:56:09 -0500

    removed works

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Intro from "./components/Intro/Intro";
 import Services from "./components/Services/Services";
 import "./App.css";
 import Experience from "./components/Experience/Experience";
-import Works from "./components/Works/Works";
+// import Works from "./components/Works/Works";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
@@ -25,7 +25,7 @@ function App() {
       <Intro />
       <Services />
       <Experience />
-      <Works />
+      {/* <Works /> */}
       <Portfolio />
       <Contact />
       <Footer />
